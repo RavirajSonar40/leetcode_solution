f14c0513c56f2a20d8ce21cb7c707b74c9eb2be4
RavirajSonar40 <ravirajsonar904@gmail.com>
2026-04-27 04:05:28 +0530

    Create 1421-FindNumbersWithEvenNumberOfDigits

---
+++ 1421-FindNumbersWithEvenNumberOfDigits/1421-FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,33 @@
+// Last updated: 27/04/2026, 04:05:25
+class Solution {
+    public int findNumbers(int[] nums) {
+         int count = 0;
+        for(int num : nums) {
+            if (even(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+   boolean even(int num) {
+        int numberOfDigits = digits2(num);
+        /*
+        if (numberOfDigits % 2 == 0) {
+            return true;
+        }
+        return false;
+         */
+        return numberOfDigits % 2 == 0;
+    }
+
+  int digits2(int num) {
+        if (num < 0) {
+            num = num * -1;
+        }
+        return (int)(Math.log10(num)) + 1;
+    }
+
+
+    
+}
