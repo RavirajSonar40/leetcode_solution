29f236b728403856b4b064fa483b890ab3e46dd8
RavirajSonar40 <ravirajsonar904@gmail.com>
2026-04-27 04:05:20 +0530

    Create 2058-ConcatenationOfArray

---
+++ 2058-ConcatenationOfArray/2058-ConcatenationOfArray.java
@@ -0,0 +1,12 @@
+// Last updated: 27/04/2026, 04:05:18
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length*2];
+
+       for(int i=0;i<nums.length;i++){
+           ans[i]=nums[i];
+           ans[i+nums.length]=nums[i];
+       }
+       return ans; 
+    }
+}
