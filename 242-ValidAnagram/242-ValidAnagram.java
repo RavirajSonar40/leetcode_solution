5039f35baf0bf0178f40721d7e7fd311c40659a8
RavirajSonar40 <ravirajsonar904@gmail.com>
2026-04-27 04:06:02 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,15 @@
+// Last updated: 27/04/2026, 04:06:00
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) freq[c - 'a']++;
+        for (char c : t.toCharArray()) freq[c - 'a']--;
+
+        for (int f : freq)
+            if (f != 0) return false;
+
+        return true;
+    }
+}
