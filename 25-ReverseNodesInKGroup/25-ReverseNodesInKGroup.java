b2a2ca1f474027ce3c23d06333b5301612d18724
RavirajSonar40 <ravirajsonar904@gmail.com>
2026-04-27 04:06:35 +0530

    Create 25-ReverseNodesInKGroup

---
+++ 25-ReverseNodesInKGroup/25-ReverseNodesInKGroup.java
@@ -0,0 +1,80 @@
+// Last updated: 27/04/2026, 04:06:33
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+    if(k <= 1 || head == null) return head;
+
+    ListNode initial = head;
+    ListNode finalHead = null;
+    ListNode lastTail = null;
+
+    while(initial != null){
+
+       
+        ListNode breaknode = initial;
+        int count = 0;
+
+        while(count < k - 1 && breaknode != null){
+            breaknode = breaknode.next;
+            count++;
+        }
+
+     
+        if(breaknode == null){
+            if(lastTail != null) lastTail.next = initial;
+            break;
+        }
+
+       
+        ListNode temp = breaknode.next;
+
+        
+        breaknode.next = null;
+
+       
+        ListNode newHead = reverse(initial);
+
+    
+        if(finalHead == null){
+            finalHead = newHead;
+        }
+
+        if(lastTail != null){
+            lastTail.next = newHead;
+        }
+
+        lastTail = initial;
+
+        initial = temp;
+    }
+
+    return finalHead;
+}
+    
+
+    static public ListNode reverse(ListNode head){
+        ListNode prev=null;
+        ListNode pres=head;
+        ListNode next=null;
+
+        while(pres!=null){ 
+            next=pres.next;
+            pres.next=prev;
+            prev=pres;
+            pres=next;
+        }
+
+        return prev;
+    }
+}
+    
+
