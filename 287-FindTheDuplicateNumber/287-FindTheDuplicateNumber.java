c8e822695e6dc886fbd6babb3aa4fae5a071879f
RavirajSonar40 <ravirajsonar904@gmail.com>
2026-04-27 04:05:59 +0530

    Create 287-FindTheDuplicateNumber

---
+++ 287-FindTheDuplicateNumber/287-FindTheDuplicateNumber.java
@@ -0,0 +1,32 @@
+// Last updated: 27/04/2026, 04:05:57
+class Solution {
+    public int findDuplicate(int[] arr) {
+        cyclicsort(arr);
+         int i = 0;
+        while (i < arr.length) {
+            if (arr[i] != i + 1) {
+                return arr[i];
+            }
+            i++;
+        }
+        return -1;
+    }
+    static void cyclicsort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if ( arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+    
+}
